99c89a7ce314c1ce1989df058d04a5cdce243382
Viacheslav <158552641+ViacheslavGonsales@users.noreply.github.com>
2024-02-17 10:14:54 +0300

    добавил папку task 1

---
+++ task_1/Program.cs
@@ -0,0 +1,13 @@
+﻿// Задача 1: Напишите программу, которая принимает на вход число 
+// и проверяет, кратно ли оно одновременно 7 и 23.
+
+Console.Write("Введите число a: ");
+int a = Convert.ToInt32(Console.ReadLine());
+if (a % 7 == 0 && a % 23 == 0)
+{
+    Console.WriteLine("да");
+}
+else
+{
+    Console.WriteLine("нет");
+}
